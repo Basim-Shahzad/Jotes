938bdaf4810315b0e1a04e45a7edd1e5ead6ce6c
G-PaperChain <g.paperchain@gmail.com>
2026-01-15 14:10:02 +0300

    Remove old GitHub Pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-   base: "/Jotes/", // <-- ADD THIS LINE
    plugins: [react(), tailwindcss()],
    define: {
       "process.env": {},
