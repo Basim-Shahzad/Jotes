40974ba381fb55cb3b67c25e9f1a566d8542b9d2
G-PaperChain <g.paperchain@gmail.com>
2026-01-15 13:57:01 +0300

    fix: update base path in vite.config.js to match project structure

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-   base: "/frontend/", // <-- ADD THIS LINE
+   base: "/Jotes/", // <-- ADD THIS LINE
    plugins: [react(), tailwindcss()],
    define: {
       "process.env": {},
